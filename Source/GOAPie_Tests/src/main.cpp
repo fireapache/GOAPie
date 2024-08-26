2ab2a43c40f21b67da06b314296b15090f0f3e1b
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2024-08-26 18:15:01 +0300

    Better main code

--- Source/GOAPie_Tests/src/main.cpp
+++ Source/GOAPie_Tests/src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
 
 // example 1
 int fundamentals();
@@ -10,6 +11,8 @@ int cutDownTrees();
 
 int main( int argc, char** argv )
 {
+	std::vector< int(*)() > funcs { fundamentals, openDoor, cutDownTrees };
+
 	int ex = -1;
 
 	if( argc > 1 )
@@ -23,7 +26,7 @@ int main( int argc, char** argv )
 			std::printf( "Enter valid example number [1..3]: " );
 			std::scanf( "%d", &ex );
 
-			if( ex < 1 || ex > 3 )
+			if( ex < 1 || ex > funcs.size() )
 			{
 				std::printf( "Wrong example number!" );
 			}
@@ -34,22 +37,7 @@ int main( int argc, char** argv )
 		}
 	}
 	
-	int exResult = 1;
-
-	switch( ex )
-	{
-	case 1:
-		exResult = fundamentals();
-		break;
-	case 2:
-		exResult = openDoor();
-		break;
-	case 3:
-		exResult = cutDownTrees();
-		break;
-	default:
-		break;
-	}
+	int exResult = funcs[ ex - 1 ]();
 
 	return exResult;
 }
